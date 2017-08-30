9c7efde53c9f10f765a9dc229d581599daecbbd3
Kelvin Oenning <kelvinstang@hotmail.com>
2017-08-30 12:48:07 -0300

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
-<template>
-  <div>
-    <my-nav></my-nav>
-    <div id="app">
-      <router-view></router-view>
-    </div>
-  </div>
-</template>
+<template src="./app.html"></template>
 
 <script>
 import Navbar from '@/components/Navbar'
@@ -17,16 +10,4 @@ export default {
 }
 </script>
 
-<style>
-html, body{
-  margin: 0px;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-}
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="stylus" src="./app.styl"></style>
